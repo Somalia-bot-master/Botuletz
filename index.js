fc74c84d2fa3dc19cfab225037711434049e5108
Somalia-bot-master <62428169+Somalia-bot-master@users.noreply.github.com>
2020-06-22 14:02:13 +0300

    upd

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-/** const Canvas = require('canvas');**/
+const Canvas = require('canvas');
 const randomPuppy = require('random-puppy');
-const superagent = require('superagent');
+
 const prefix = 'ba';
 
 const token = 'Njg5NzU4NTY3NjUzNTcyNzgy.XqaxMg.W8U218G1ltlAFcHPZfJhXhwlmf0';
@@ -204,7 +204,7 @@ bot.on('message', async message => {
       case 'chainsaw':
         message.reply("BRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR");
         break;
-      /**case 'ricesays':
+      case 'ricesays':
         if(args[1]==null)message.channel.send("zi ce sa zica orezu!")
         else
         {
@@ -218,6 +218,7 @@ bot.on('message', async message => {
         ctx.drawImage(background , 0,0, canvas.width,canvas.height);
         /** const background = await loadImage('./rice.png');
         ctx.drawImage(background , 0,0, canvas.width,canvas.height);
+        **/
         
         // Write "Awesome!"
         ctx.font = '40px Sans'
@@ -231,7 +232,7 @@ bot.on('message', async message => {
         case 'jebaited':
       message.channel.send("U got jebaited", { files: ["./test.gif"] });
       message.channel.send({ files: ["./test.gif"] });
-      break;**/
+      break;
        case 'meme':
          let reddit=[
            "dankmemes",
